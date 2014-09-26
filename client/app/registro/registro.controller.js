77920ce52007ffa865d578ecbcb84b19e6277dce
manuelxaguilar <manuelxaguilar@gmail.com>
2014-09-26 10:21:41 -0600

    Cambios en registro, se anadieron las ultimos dos pasos. Ademas cambios en registro.controller

--- client/app/registro/registro.controller.js
+++ client/app/registro/registro.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('modulusIoApp')
-  .controller('RegistroCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('RegistroCtrl', function ($scope, $timeout, $location) {
+    $scope.message = false;
+
+    $scope.timeout =  function( ) {
+    	$timeout (function () {
+		$scope.mensaje = true;
+		$timeout (function () {
+			$location.path('/login');
+		}, 3000);
+	}, 2000)
+    };
+ });
